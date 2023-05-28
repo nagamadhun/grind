1667c80ea2bdaa2ba90ff596ade7413c2ccb52eb
nagamadhun <nadakudurunagamadhu@gmail.com>
2023-05-28 23:55:28 +0530

    Explore nearby

--- airbnb_clone/pages/index.js
+++ airbnb_clone/pages/index.js
@@ -1,5 +1,6 @@
 import Banner from '@/components/Banner'
 import Header from '@/components/Header'
+import SmallCard from '@/components/SmallCard'
 
 
 export default function Home({exploreData}) {
@@ -10,9 +11,14 @@ export default function Home({exploreData}) {
     <main className='max-w-7xl mx-auto px-8 sm:px-16'>
       <section className='pt-6'>
         <h2 className='text-4xl font-semibold'>Explore Nearby</h2>
-        {exploreData.map((i) => (
-          <h1>{i.location}</h1>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {exploreData.map(({img,location,distance}) => (
+          <SmallCard key={img}
+           img={img} location={location}
+           distance={distance}/>
         ))}
+        </div>
+       
       </section>
     </main>
   </div>
